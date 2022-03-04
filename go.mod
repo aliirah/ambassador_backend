519cbd5e21153e9e237e4189aa02491091aad36b
vahiidrah <vahiid_rah@yahoo.com>
2022-03-04 11:36:53 +0330

    init commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module alirah
+
+go 1.16
+
+require github.com/gofiber/fiber/v2 v2.28.0 // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
+
+	err := app.Listen(":8000")
+	if err != nil {
+		log.Fatalln("error serving")
+	}
+}
