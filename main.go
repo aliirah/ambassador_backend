ce3896e767d74dce00e60b976bb4084098fe1683
vahiidrah <vahiid_rah@yahoo.com>
2022-03-04 12:18:27 +0330

    add postgresql database

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"alirah/config"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"log"
 )
 
 func main() {
+	dsn := config.Dns()
+	_, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect with the database!")
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World!")
 	})
 
-	err := app.Listen(":8000")
+	err = app.Listen(":8000")
 	if err != nil {
 		log.Fatalln("error serving")
 	}
