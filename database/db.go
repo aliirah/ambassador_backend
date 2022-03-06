3720bdb5ea271c2aee336073ff58fb08f241116c
vahiidrah <vahiid_rah@yahoo.com>
2022-03-06 14:50:15 +0330

    fix db

--- database/db.go
+++ database/db.go
@@ -1,18 +1,35 @@
 package database
 
 import (
-	"alirah/config"
-	"alirah/domain"
+	"alirah/app/domain"
+	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
+	"strconv"
 )
 
 var DB *gorm.DB
 
 func Connect() {
 	var err error
-	dsn := config.Dns()
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var (
+		host     = os.Getenv("POSTGRES_HOST")
+		port, _  = strconv.Atoi(os.Getenv("POSTGRES_PORT"))
+		user     = os.Getenv("POSTGRES_USER")
+		password = os.Getenv("POSTGRES_PASSWORD")
+		dbname   = os.Getenv("POSTGRES_DB")
+	)
+
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
